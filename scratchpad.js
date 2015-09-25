e8cb39fde1ddddf22b9cbcc722cae1705667175e
Prasad Pawar <ubuntu.prasad@gmail.com>
2015-09-25 19:27:45 +0530

    Add from Linux

---
+++ scratchpad.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+<html>
+  <head>
+  <title> Firefox Developer Tool </title>
+  <script type="text/javascript">
+    document.write("This is firefox developer toolbox test program...");
+  </script>
+  </head>
+  <body>
+  </body>
+</html>
+  
